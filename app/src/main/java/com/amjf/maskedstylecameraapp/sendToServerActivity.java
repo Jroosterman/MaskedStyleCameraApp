43df5b90bf501db649eabfd3aefd72347f3f7705
jroosterman <jroosterman48@gmail.com>
2019-06-02 18:26:07 -0400

    Change

--- app/src/main/java/com/amjf/maskedstylecameraapp/sendToServerActivity.java
+++ app/src/main/java/com/amjf/maskedstylecameraapp/sendToServerActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
